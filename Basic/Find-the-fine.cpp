46ca9b6219783622a16afb2bea4858b0e0b25560
Himanshu Chauhan <codebreaker8055@gmail.com>
2023-02-05 22:19:30 +0530

    Find the fine

---
+++ Basic/Find-the-fine.cpp
@@ -0,0 +1,108 @@
+/*Given an array of penalties fine[], an array of car numbers car[], and also the date. The task is to find the total fine which will be collected on the given date. Fine is collected from odd-numbered cars on even dates and vice versa.
+
+
+
+Example 1:
+
+Input:
+N = 4, date = 12
+car[] = {2375, 7682, 2325, 2352}
+fine[] = {250, 500, 350, 200}
+Output:
+600
+Explantion:
+The date is 12 (even), so we collect the
+fine from odd numbered cars. The odd
+numbered cars and the fines associated
+with them are as follows:
+2375 -> 250
+2325 -> 350
+The sum of the fines is 250+350 = 600
+
+
+Example 2:
+
+Input:
+N = 3, date = 8
+car[] = {2222, 2223, 2224}
+fine[] = {200, 300, 400}
+Output:
+300*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+public:
+    long long int totalFine(int n, int date, int car[], int fine[])
+    {
+        long long int Ans = 0;
+
+        if (date % 2 == 0)
+
+        {
+
+            for (int i = 0; i < n; i++)
+
+            {
+
+                if (car[i] % 2 != 0)
+
+                {
+
+                    Ans += fine[i];
+                }
+            }
+        }
+
+        else
+
+        {
+
+            for (int i = 0; i < n; i++)
+
+            {
+
+                if (car[i] % 2 == 0)
+
+                {
+
+                    Ans += fine[i];
+                }
+            }
+        }
+
+        return Ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, i, date;
+        cin >> n >> date;
+        int car[n], fine[n];
+
+        for (i = 0; i < n; i++)
+        {
+            cin >> car[i];
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cin >> fine[i];
+        }
+        Solution obj;
+        cout << obj.totalFine(n, date, car, fine) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
